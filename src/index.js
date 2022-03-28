460158e0967c2accfd3138f26f2f7908a5261a1c
Joynal Dsouza <95961916+JoynalDsouza@users.noreply.github.com>
2022-03-28 11:04:35 +0530

    style : padding fix in Menu Component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
+
+const Wrapper = ({ children }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children;
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Wrapper>
+        <App />
+      </Wrapper>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
